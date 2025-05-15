f5e0e013150b1ff872d27e7c0180a9b81454ce54
petersonbabs <timiebabs@gmail.com>
2025-05-15 12:52:18 +0100

    dev

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,2 +1,25 @@
 // create a function: displayNum(num) that receives the parameter of the number that was clicked and display it on the input
+const inputEl = document.getElementById("input")
+let canClear = false
 
+// save the first set of numbers in a variable= > firstVal
+// save the operator that was clicked in a vaiabl => operator
+
+function displayNum(num){
+    if(canClear){
+        inputEl.value = ""
+        inputEl.value += num
+        canClear = false
+    }else{
+        inputEl.value += num
+    }
+}
+
+function operator(){
+    canClear = true
+}
+
+
+function equalTo(){
+    // log the firstValue, Operator, and the secondValue
+}
