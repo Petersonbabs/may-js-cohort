142650ff466795fc24261b4d89ce7a8bbe941779
petersonbabs <timiebabs@gmail.com>
2025-05-19 13:04:31 +0100

    dev

---
+++ arrays_&_object/script.js
@@ -0,0 +1,96 @@
+// 
+const fruits = ["orange", "mango", "banana", "pawpaw", 78, 10, false]
+// console.log(fruits)
+
+// length
+// element
+// index
+
+
+// methods: modify, create a new array 
+// fruits.pop()
+// fruits.push("carrot")
+// fruits.shift()
+// fruits.unshift("lemon")
+// console.log(fruits)
+
+
+// fruits.push("orange");     // Adds to end
+// fruits.pop();              // Removes from end
+// fruits.shift();            // Removes from beginning
+// fruits.unshift("lemon");   // Adds to beginning
+// fruits.includes("banana"); // true/false
+// fruits.indexOf("mango");   // 1 (index)
+
+
+// const students = ["ade", "bola", "john"]
+
+// students.unshift("kjhgf")
+// students.pop()
+// students.shift()
+// students.push("liukyjhfgv")
+// console.log(students)
+
+// const names = [9, true, "kemi","kemi", "kemi","kemi", "borne", "nobd"]
+// const nameIndex = names.indexOf("kemi")
+// console.log(nameIndex)
+
+// promp the user for a name and log the index number of the name
+
+// names.includes("sisi")
+
+// const array = [
+//     "peter", // 0
+//     [90, 290, 50, 60], // 1
+//     "kemi", // 2
+//     [true, false, ["ife", "james"]] // 3
+    
+// ]
+
+// console.log(array[3][2][0])
+
+
+
+function pick(){
+
+}
+
+// s p = com won
+// s r = user won
+// p r = com won
+
+// const options = ["scissors", "paper", "rock"]
+
+// const com = options[Math.floor(Math.random() * options.length )]
+
+// console.log(com)
+
+const user2 = ["james", 40, "Nigeria"]
+
+const user = {
+    name: "james",
+    age: 40,
+    address: "Nigeria",
+    scores: [90, 67, 89]
+}
+
+user.name =  "paul"
+user.phone = 89
+// console.log(user)
+
+
+const users = [
+    {name: "Jame", age: 89, Address: "Nigeria"},
+    {name: "Ade", age: 3, Address: "Nigeria"},
+    {name: "Ali", age: 4, Address: "Nigeria"},
+    {name: "Paul", age: 5, Address: "Nigeria"},
+    {name: "Jamessss", age: 896, Address: "Nigeria"},
+    {name: "sgege", age: 8, Address: "Nigeria"},
+]
+
+// const chosenNum = prompt("Choose a number")
+const username = users[chosenNum].name
+// console.log(username)
+// console.log(users[2].Address)
+
+
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -5,6 +5,7 @@ let canClear = false
 let firstValue;
 let secondValue;
 let operatorSign;
+let operatorClicked = 0
 
 // save the operator that was clicked in a vaiabl => operator
 
@@ -20,14 +21,29 @@ function displayNum(num) {
 
 function operator(sign) {
     // save the first set of numbers in a variable = > firstVal
-    firstValue = inputEl.value
     operatorSign = sign
+    operatorClicked++ // 1, 2
     canClear = true
+    if(operatorClicked > 1 && inputEl.value){
+        console.log(firstValue, inputEl.value)
+        performArithmetic(sign)
+    } else{
+        firstValue = inputEl.value
+    }
 }
 
+function performArithmetic(sign){
+    console.log(sign)
+if(sign === "+"){
+    inputEl.value = umberN(firstValue) + Number(inputEl.value)     
+}
+
+}
 
 function equalTo() {
     secondValue = inputEl.value
+
+    
     console.log(firstValue, operatorSign, secondValue)
     // log the firstValue, Operator, and the secondValue
 }
